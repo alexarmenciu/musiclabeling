2de3922a03c68c0fcb88a9af248e28780cb87b92
Alex Armenciu <alex.armenciu@gmail.com>
2022-05-09 15:00:52 -0400

    created environment setup for access and secret api keys

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
+import dotenv
 import pydub
+import dotenv
 
 # going to make this file a little monolithic, since i want this to be as user friendly as possible so i don't want to have
 # the user cart around two scripts wherever they need the functionality
+
+dotenv.load_dotenv()
+
+acrcloud_access_key = os.environ.get("ACR_ACCESS")
+acrcloud_access_secret = os.environ.get("ACR_SECRET")
+acrcloud_request_url = os.environ.get("ACR_URL")
 
 
 def get_music_files():
@@ -21,15 +29,21 @@ def get_music_files():
 def reduce_file_size(files):
     """
     Creates a temporary folder with the music files at a reduced size since the acrcloud service accepts <1mb files
+    returns no output
     
     Args:
         files (file list): list of music files to be reduced in size
     """
     os.makedirs("tempMusicStorage")
 
+    # iterate through each file, cut to 15 sec, save in temp folder
     for file in files:
-        if f.endswith(".mp3"):
-            audio = pydub.AudioSegment.from_mp3()
+        if file.endswith(".mp3"):
+
+            audio = pydub.AudioSegment.from_mp3(file=file)
+            first_fifteen = audio[:15000]
+
+            first_fifteen.export("./tempMusicStorage/" + file, format=".mp3")
 
 
 def query_acrcloud(reduced_music_files):
@@ -40,4 +54,18 @@ def query_acrcloud(reduced_music_files):
         reduced_music_files (file list): list of music files below 1mb to be identified by acrcloud
     """
 
+    for file in os.listdir("./tempMusicStorage/"):
+        print(1)
+
     return []
+
+
+def close_temp_folder():
+    """
+    closes temporary folder created by the above functions
+    doesnt take any inputs
+    
+    :)
+    """
+
+    os.remove("./tempMusicStorage")
