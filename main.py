f2f67ef3c69f1a85b242373f59daaca796b90b2e
Alex Armenciu <alex.armenciu@gmail.com>
2022-05-09 16:05:28 -0400

    worked on post request for song metadata

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import os
 import dotenv
 import pydub
 import dotenv
+import time
+import base64
+import hashlib
+import hmac
 
-# going to make this file a little monolithic, since i want this to be as user friendly as possible so i don't want to have
+# going to make this project monolithic, since i want this to be as user friendly as possible so i don't want to have
 # the user cart around two scripts wherever they need the functionality
 
 dotenv.load_dotenv()
@@ -54,10 +58,29 @@ def query_acrcloud(reduced_music_files):
         reduced_music_files (file list): list of music files below 1mb to be identified by acrcloud
     """
 
-    for file in os.listdir("./tempMusicStorage/"):
-        print(1)
+    files_with_metadata = []
 
-    return []
+    # prepare post request with necessary data
+
+    http_method = "POST"
+    http_uri = "/v1/identify"
+    data_type = "audio"
+    signature_version = "1"
+
+    for file in os.listdir("./tempMusicStorage/"):
+        #further preparation for post request, required to be inside the loop so as the timestamp will change with each file processed
+
+        timestamp = time.time()
+
+        string_to_sign = http_method + "\n" + http_uri + "\n" + acrcloud_access_key + "\n" + data_type + "\n" + signature_version + "\n" + str(
+            timestamp)
+
+        sign = base64.b64encode(
+            hmac.new(acrcloud_access_secret.encode('ascii'),
+                     string_to_sign.encode('ascii'),
+                     digestmod=hashlib.sha1).digest()).decode('ascii')
+
+    return files_with_metadata
 
 
 def close_temp_folder():
